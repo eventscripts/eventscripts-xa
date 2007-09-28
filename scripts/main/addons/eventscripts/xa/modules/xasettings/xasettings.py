188d51f497528f1851c5eb4b2e9f40d8638639ae
mback2k <mback2k@localhost>
2007-09-28 15:29:37 +0000

    Unfinished xasettings.py

--- scripts/main/addons/eventscripts/xa/modules/xasettings/xasettings.py
+++ scripts/main/addons/eventscripts/xa/modules/xasettings/xasettings.py
@@ -17,6 +17,8 @@ info.url            = "http://forums.mattie.info"
 info.description    = "Clone of Mani Player Settings feature for XA"
 info.tags           = "admin settings players"
 
+setting_object = {}
+
 xasettings                  = xa.register('xasettings')
 xalanguage                  = xa.language.getLanguage(xasettings)
 
@@ -31,4 +33,37 @@ def unload():
     xa.unRegister(xasettings)
 
 def _select_setting(userid, choice, name):
+    pass
+    
+class Setting_switch(object):
+    def __init__(self, setting, options, texts):
+        self.name = setting
+        self.texts = texts
+        self.options = options
+    def use(self, userid):
+        pass
+
+class Setting_menu(object):
+    def __init__(self, setting, menu, texts):
+        self.name = setting
+        self.texts = texts
+        if popuplib.exists(menu):
+            self.menu = menu
+            self.menutype = "popup"
+            self.menuobj = popuplib.find(self.menu)
+        elif keymenulib.exists(menu):
+            self.menu = menu
+            self.menutype = "keymenu"
+            self.menuobj = keymenulib.find(self.menu)
+        elif settinglib.exists(menu):
+            self.menu = menu
+            self.menutype = "setting"
+            self.menuobj = settinglib.find(self.menu)
+    def use(self, userid):
+        pass
+        
+def registerSetting(setting, options, texts):
+    pass
+        
+def registerSubmenu(setting, menu, texts):
     pass
