8e25ca6d92710d6f718d650e75252a9b63cea6f0
mback2k <mback2k@localhost>
2007-09-28 15:17:19 +0000

    Unfinished xasettings.py

--- scripts/main/addons/eventscripts/xa/modules/xasettings/xasettings.py
+++ scripts/main/addons/eventscripts/xa/modules/xasettings/xasettings.py
@@ -1,25 +1,36 @@
 import es
 import xa
 import xa.language
+import xa.logging
 import xa.setting
 import xa.playerdata
-import xa.manilib
+import playerlib
 import popuplib
 from xa import xa
 
 #plugin information
 info = es.AddonInfo()
-info.name = "XA:Settings"
-info.version = "0.1"
-info.author = "Hunter"
-info.url = "http://forums.mattie.info"
-info.description = "Clone of Mani Player Settings feature for XA"
-info.tags = "admin settings players"
+info.name           = "XA:Settings"
+info.version        = "0.1"
+info.author         = "Hunter"
+info.url            = "http://forums.mattie.info"
+info.description    = "Clone of Mani Player Settings feature for XA"
+info.tags           = "admin settings players"
 
+xasettings                  = xa.register('xasettings')
+xalanguage                  = xa.language.getLanguage(xasettings)
 
 def load():
     #Load Function for Player Settings for XA.
-    xasettings = xa.register('xasettings')
+    xasettings = xa.find('xasettings')
+    
+    xasettingmenu = popuplib.easymenu("xasettingmenu", "_tempcore", _select_setting)
+    xasettingmenu.settitle(xalanguage["player settings"])
+    xapunishments.addMenu("xasettingmenu", xalanguage["player settings"], "xasettingmenu", "change_playersetting", "#all")
 
 def unload():
+    popuplib.delete("xasettingmenu")
     xa.unRegister('xasettings')
+
+def _select_setting(userid, choice, name):
+    pass
