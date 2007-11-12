196ff47160aa1299a0eb94e3d7030d8323786768
poormattie <poormattie@gmail.com>
2007-11-12 19:11:09 +0000

    Added an example XA module-- needs support for example menu addition, localization and player setting usage.
    Added initial pass for xasay module-- this provides initial support for admin say (with an @ symbol) but supports registering other say prefixes to do more fun things.

---
+++ scripts/main/addons/eventscripts/xa/modules/xasample/xasample.py
@@ -0,0 +1,59 @@
+import es
+import xa
+import xa.setting
+import xa.logging
+
+#######################################
+# ADDON INFORMATION
+# This describes the XA module
+info = es.AddonInfo()
+# TODO: Change this to your module's data.  -- TODO
+info.name           = "XA:Sample"
+info.version        = "0.1"
+info.author         = "New Scripter"
+info.url            = "http://forums.mattie.info/"
+info.description    = "Sample XA Module"
+
+
+#######################################
+# MODULE NAME
+# This is the name of the module.
+# TODO: Change this to your module name.    -- TODO
+mymodulename = "mymodule"
+# Register the module
+# this is a global reference to your module
+mymodule = xa.register(mymodulename)
+
+
+#######################################
+# SERVER VARIABLES
+# The list of our my server variables
+# TODO: Add your own variables              -- TODO
+myvariable = xa.setting.createVariable('some_variable', 1, "XA: Some variable (1=on, 0=off)")
+
+
+#######################################
+# GLOBALS
+# Initialize our general global data here.
+# Localization helper:
+text = xa.language.getLanguage(mymodulename)
+
+
+#######################################
+# LOAD AND UNLOAD
+# Formal system registration and unregistration
+def load():
+    xa.logging.log(mymodule, "XA module %s loaded." % mymodulename)
+    # TODO: Register menu, say, client, or console commands.
+    #                                       -- TODO
+
+
+def unload():
+    xa.logging.log(mymodule, "XA module is %s being unloaded." % mymodulename)
+    # Unregister the module
+    xa.unregister(mymodulename)
+    
+    
+#######################################
+# MODULE FUNCTIONS
+# Register your
