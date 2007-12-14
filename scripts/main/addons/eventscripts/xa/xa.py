66c7ea2b9e3781c79e67d771a089f0a613e3a24f
poormattie <poormattie@gmail.com>
2007-12-14 04:36:11 +0000

    Incrementing version number to 004 of xa

--- scripts/main/addons/eventscripts/xa/xa.py
+++ scripts/main/addons/eventscripts/xa/xa.py
@@ -37,7 +37,7 @@ xa_log = es.ServerVar("xa_log", 0, "Activates the module logging")
 ## language strings
 gLanguage = language.getLanguage()
 ## Version variable
-gVersion = es.ServerVar("eventscripts_xa", "0.0.1.000", "eXtensible Admin Version")
+gVersion = es.ServerVar("eventscripts_xa", "0.0.4.000", "eXtensible Admin Version")
 gVersion.makepublic()
 ## is Mani compatibility enabled?
 gManiMode = es.ServerVar("xa_manimode", 0, "Is Mani compatibility mode active?")
