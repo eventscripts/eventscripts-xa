e6027309a915c386c90d99f3c4f03e15445dc0eb
morton.thomas <morton.thomas@localhost>
2008-01-26 22:17:20 +0000

    - Added "import xa.setting" to Xaobjectives (to try and make it work, no luck though) - Changed tabs to be 4 spaces as per the standard :)

--- scripts/main/addons/eventscripts/xa/modules/xaobjectives/xaobjectives.py
+++ scripts/main/addons/eventscripts/xa/modules/xaobjectives/xaobjectives.py
@@ -1,5 +1,6 @@
 ﻿import es
 from playerlib import getPlayerList
+import xa.setting
 from xa import xa
 
 info = es.AddonInfo()
@@ -9,35 +10,35 @@ info.author = "Wonder"
 info.description = "Slays the team which fails to complete their objectives."
 
 def load():
-	global xaobjectives, slay, text
+    global xaobjectives, slay, text
 
-	xaobjectives = xa.register("xaobjectives")
-	slay = xa.setting.createVariable(xaobjectives, "css_objectives", "1", "XA: If 1, losing team will be slain.")
-	text = xa.language.getLanguage("xaobjectives")
+    xaobjectives = xa.register("xaobjectives")
+    slay = xa.setting.createVariable(xaobjectives, "css_objectives", "1", "XA: If 1, losing team will be slain.")
+    text = xa.language.getLanguage("xaobjectives")
 
-	if es.getgame() != "Counter-Strike: Source":
-		return False
+    if es.getgame() != "Counter-Strike: Source":
+        return False
 
 def unload():
-	xa.unregister("xaobjectives")
+    xa.unregister("xaobjectives")
 
 def round_end(eventVar):
-	if int(slay):
-		if eventVar["winner"] == "2" and es.getlivingplayercount(3):
-			cts, ts = getPlayerList("#ct"), getPlayerList("#t")
-			for i in cts:
-				i.kill()
-				es.tell(int(i), "#multi", text("lost"))
-			for i in ts:
-				es.tell(int(i), "#multi", text("won"))
-		elif eventVar["winner"] == "3" and es.getlivingplayercount(2):
-			ts, cts = getPlayerList("#t"), getPlayerList("#ct")
-			for i in ts:
-				i.kill()
-				es.tell(int(i), "#multi", text("lost"))
-			for i in cts:
-				es.tell(int(i), "#multi", text("won"))
+    if int(slay):
+        if eventVar["winner"] == "2" and es.getlivingplayercount(3):
+            cts, ts = getPlayerList("#ct"), getPlayerList("#t")
+            for i in cts:
+                i.kill()
+                es.tell(int(i), "#multi", text("lost"))
+            for i in ts:
+                es.tell(int(i), "#multi", text("won"))
+        elif eventVar["winner"] == "3" and es.getlivingplayercount(2):
+            ts, cts = getPlayerList("#t"), getPlayerList("#ct")
+            for i in ts:
+                i.kill()
+                es.tell(int(i), "#multi", text("lost"))
+            for i in cts:
+                es.tell(int(i), "#multi", text("won"))
 
 def round_start(eventVar):
-	if int(slay):
-		es.msg("#multi", text("round_start"))
+    if int(slay):
+        es.msg("#multi", text("round_start"))
