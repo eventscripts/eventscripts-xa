08f4f6e64a6744b2ce05d4391492fc8454d117e2
morton.thomas <morton.thomas@localhost>
2008-02-04 20:42:54 +0000

    Updated xaipghosting to 1.2.0:  * Added option to blind spectators who are IP ghosting  * Added quick catchem for players leaving the server  * Reworked some of the methods in a minor way. Next up:  * Add a console / say command

--- scripts/main/addons/eventscripts/xa/modules/xaipghosting/xaipghosting.py
+++ scripts/main/addons/eventscripts/xa/modules/xaipghosting/xaipghosting.py
@@ -13,7 +13,7 @@ from xa import xa
 #plugin information
 info = es.AddonInfo()
 info.name = "IP Ghosting"
-info.version = "1.1.0a"
+info.version = "1.2.0"
 info.author = "Errant"
 info.url = "http://forums.mattie.info/cs/forums/viewtopic.php?t=16321"
 info.description = "Clone of Mani's IP ghosting feature for XA"
@@ -34,15 +34,19 @@ OY1   | [BETA] | 15/09/2007 |  Working Standalone version
 1.0.1 | [FULL] | 15/11/2007 | [FIX] r_screenoverlay requires a cheat - changed to use 1 1sec repeated fade via usermsg.fade (works v well - thx to Mattie for the idea)
 1.1.0 | [FULL] | 21/01/2008 | Changed to using a class for the blind, fixed a few idiot errors and cleaned things up a bit. Added a cvar to show the version publicly. Added logging of the blind
 1.1.1 | [FULL] | 21/01/2008 | Lots of silly fixes (thx mattie) - and fixed a repeat problem that stopped it working
+1.2.0 | [FULL] | 04/02/2008 | Added option to blind spectators who are IP ghosting, Added catchem for players leaving the server, Reworked some of the methods in a minor way.
 
 
 --Future--
  #  |  Status       | Desc
+1.3 | [UNSTARTED]   | Add a console command
 1.5 | [UNSTARTED]   | Add other features, provide admin notification / features, add further config options
 '''
 
 # the dictionary to track blinded players
 blinded = {}
+# same idea but for spectators
+spec_blinded = {}
 
 # Grab the languages file using the XA langlib wrapper
 text = xa.language.getLanguage('xaipghosting')
@@ -81,7 +85,13 @@ def repeat_fade(x):
     '''
     for uid in blinded:
         blinded[uid].blind()
-
+        
+def repeat_fade(x):
+    '''
+    As above but for the spectators dict
+    '''
+    for uid in spec_blinded:
+        spec_blinded[uid].blind()
 
 def blindplayer(uid):
     '''
@@ -100,6 +110,22 @@ def blindplayer(uid):
     blinded[uid].tell_blinded()
     # log that they were blinded
     xa.logging.log(ghosting, "Blinded player %s (%s)" % (str(uid), es.getplayersteamid(uid)))
+                
+def blindplayer_spec(uid):
+    '''
+    As above but for the spectator Dict
+    '''
+    global spec_blinded
+    if repeat.status("xaip_spec") == 0:
+        a = repeat.create("xaip_spec", repeat_fade_spec)
+        a.start(1,0)
+    # do the initial fade 
+    spec_blinded[uid] = Player(uid)
+    spec_blinded[uid].blind()
+    # tell them
+    spec_blinded[uid].tell_blinded()
+    # log that they were blinded
+    xa.logging.log(ghosting, "Blinded player %s (%s)" % (str(uid), es.getplayersteamid(uid)))
     
 
 def checkplayer(uid):
@@ -111,7 +137,21 @@ def checkplayer(uid):
         uip = plist[int(uid)]["address"]   
         for id in plist:
             if not id == int(uid) and plist[id]["address"]== uip: 
-                blindplayer(uid) 
+                return True
+    return False
+    
+def remove_from_spec(uid):
+    '''
+    Removes the user from the spec dict and stops the repeat if the dict is empty
+    '''
+    global spec_blinded
+    spec_blinded.pop(event_var["userid"])
+        if len(spec_blinded) < 1:
+            # no one IP ghosting as spec so kill the repeat
+            if repeat.status("xaip_spec") > 0:
+                r = repeat.find("xaip_spec")
+                r.stop()
+                r.delete()
 '''
 Events
 '''
@@ -121,6 +161,10 @@ def load():
     ghosting = xa.register("xaipghosting") 
     # sort the variable registration
     xa.setting.createVariable(ghosting, 'blind_ghosters', '1', "Blind IP Ghosters when they die (1=On, 0=Off)") 
+    # Option to blind people who are speccing
+    xa.setting.createVariable(ghosting, 'blind_ghosters_when_spectating', '1', "Blind IP Ghosters when they are spectating (1=On, 0=Off)") 
+    # create the console command
+    # NOT DONE YET
     # log what happened
     xa.logging.log(ghosting, "Loaded IP Ghosting (mani clone) V%s" % (info.version))
 
@@ -129,8 +173,9 @@ def player_death(event_var):
     '''
     If player is IP ghosting add them to the list! And if the repeat is not fired yet then do that too!
     '''
-    if xa.setting.getVariable(ghosting, 'blind_ghosters') != "0":
-        checkplayer(event_var['userid'])
+    if xa.setting.getVariable(ghosting, 'blind_ghosters') != "0" and checkplayer(event_var['userid']):
+        blindplayer(event_var['userid'])
+        
 
 def round_end(event_var):
     '''
@@ -142,7 +187,28 @@ def round_end(event_var):
         r.stop()
         r.delete()
     blinded = {}
-
+    
+def player_team(event_var):
+    '''
+    Blinds a ghoster when they switch to Spec.. and removes them again when they go back to playing.
+    '''
+    if xa.setting.getVariable(ghosting, 'blind_ghosters_when_spectator') != "0" and checkplayer(event_var['userid']) and int(vent_var['team']) == 1:
+        blindplayer_spec(event_var['userid'])
+    if int(vent_var['team']) > 1:
+        # check they are not on the list and remove them
+        remove_from_spec(event_var['userid'])
+        
+def player_disconnect(event_var):
+    '''
+    When a player disconnects remove them fro mthe dictionaries (to avoid errors)
+    '''
+    if event_var["userid"] in blinded:
+        # player is currently beign blinded
+        blinded.pop(event_var["userid"])
+    if event_var["userid"] in spec_blinded:
+        # player is currently beign blinded
+        remove_from_spec(event_var['userid'])
+            
 def unload():
     xa.logging.log(ghosting, "XA module xaipghosting is being unloaded.")
     # Unregister the module
