fab39664108d4366caa0f2374180230b03bbdcb1
Davidthegenius <Davidthegenius@localhost>
2008-03-25 21:17:21 +0000

    xatimeleft fix - http://forums.mattie.info/cs/forums/viewtopic.php?p=193653#193653

--- scripts/main/addons/eventscripts/xa/modules/xatimeleft/xatimeleft.py
+++ scripts/main/addons/eventscripts/xa/modules/xatimeleft/xatimeleft.py
@@ -51,6 +51,9 @@ def timeleft_cmd():
    float_mp_timelimit = float(es.ServerVar('mp_timelimit')) * 60
    if float_mp_timelimit:
       float_time_left = float_mp_timelimit - (time.time() - float_map_start_time)
+      if float_time_left < 0: float_time_left = 0
+
       es.tell(int_userid, 'Time left in map: %s:%s' % (int(float_time_left / 60), int(float_time_left % 60)))
+
    else:
       es.tell(int_userid, 'No map time limit')
