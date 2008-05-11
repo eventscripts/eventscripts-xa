cc06d36bd819befeb97abe9256db267efd8c7450
mback2k <mback2k@localhost>
2008-05-11 18:07:55 +0000

    Fixed: When you try and send a message to a non-existing player. Instead of displaying "Sorry, Player (TEXT) was not found." It displays (TEXT) as "0" instead of what the text of player you typed in.

--- scripts/main/addons/eventscripts/xa/modules/xaextendedsay/xaextendedsay.py
+++ scripts/main/addons/eventscripts/xa/modules/xaextendedsay/xaextendedsay.py
@@ -38,9 +38,9 @@ def _admin_say_tell(adminid, message, teamonly):
             es.centertell(userid , xalanguage('admin center to player', tokens, playerlib.getPlayer(userid).get("lang"))) 
             es.centertell(adminid, xalanguage('admin center to player', tokens, playerlib.getPlayer(adminid).get("lang"))) 
     else: 
-        tokens['userid'] = userid 
+        tokens['username'] = message.split()[0]
         es.tell(adminid, '#multi', xalanguage('no player found', tokens, playerlib.getPlayer(adminid).get("lang"))) 
-    return (0,'',0) 
+    return (0,'',0)
     
 def _admin_say_center(userid, message, teamonly): 
     tokens = {} 
