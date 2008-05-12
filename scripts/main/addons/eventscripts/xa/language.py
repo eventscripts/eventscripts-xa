b9469f124708c90ea1f466838d72d4fbd84b2970
mback2k <mback2k@localhost>
2008-05-12 09:49:49 +0000

    Introduced new language.py LanguageDict class to reduce the memory usage by providing a fake class for language dicts instead of a dict with dummy strings for other languages. Fixed little mistake in strings.ini

--- scripts/main/addons/eventscripts/xa/language.py
+++ scripts/main/addons/eventscripts/xa/language.py
@@ -1,5 +1,6 @@
 import es
 import os
+import copy
 import langlib
 
 import psyco
@@ -11,21 +12,83 @@ psyco.full()
 # All methods that should be able to be called through #
 # the API need to have "module" as first parameter     #
 ########################################################
-def createLanguageString(module = None, string = None):
-    if not string:
-        string = module
-    if string:
-        full = langlib.getLanguages()
-        lang = {}
-        ####################################################################
-        ## ADD LANGUAGE PLACEHOLDERS FOR EASYMENUS -- BAD WAY -> OVERHEAD ##
-        ####################################################################
-        for l in full:
-            i = full[l]["id"]
-            if not i in lang:
-                lang[i] = str(string)
-        ####################################################################
-        return lang
+class LanguageDict(dict):
+    languages = langlib.getLanguages()
+    abbreviations = map(langlib.getLangAbbreviation, languages)
+    defaultlang = langlib.getDefaultLang()
+    def __init__(self, key, value):
+        self.name = key
+        if isinstance(value, dict):
+            self.text = self.mapdict(str, value)
+        else:
+            self.text = {self.defaultlang:str(value)}
+    def __str__(self):
+        return ("LanguageDict(%s)" % self.name)
+    def __repr__(self):
+        return self.__str__
+    def __len__(self):
+        return len(self.abbreviations)
+    def __getitem__(self, key):
+        if key in self.abbreviations:
+            if key in self.text:
+                return self.text[key]
+            elif self.defaultlang in self.text:
+                return self.text[self.defaultlang]
+            elif "en" in self.text:
+                return self.text["en"]
+            else:
+                raise IndexError("LanguageDict(%s): No language string available" % self.name)
+        else:
+            raise IndexError("LanguageDict(%s): No valid abbreviation" % self.name)
+    def __setitem__(self, key, value):
+        if key in self.abbreviations:
+            self.text[key] = str(value)
+        else:
+            raise IndexError("LanguageDict(%s): No valid abbreviation" % self.name)
+    def __delitem__(self, key):
+        if key in self.abbreviations:
+            if key != self.defaultlang and key != "en":
+                del self.text[key]
+            else:
+                raise ValueError("LanguageDict(%s): You can't delete the default keys" % self.name)
+        else:
+            raise IndexError("LanguageDict(%s): No valid abbreviation" % self.name)
+    def __iter__(self):
+        return self.abbreviations.__iter__()
+    def __contains__(self, key):
+        if key in self.abbreviations:
+            return True
+        else:
+            return False
+    def clear(self):
+        pass
+    def copy(self):
+        return copy.copy(self)
+    def has_key(self, key):
+        return self.__contains__(key)
+    def items(self):
+        return self.text.items()
+    def keys(self):
+        return self.abbreviations
+    def update(self, iterable):
+        return self.text.update(iterable)
+    def fromkeys(self, seq, value = None):
+        return self.text.fromkeys(seq, value)
+    def values(self):
+        return self.text.values()
+    def mapdict(self, function, iterable):
+        if callable(function) and iterable:
+            for key in iterable:
+                iterable[key] = function(iterable[key])
+            return iterable
+        else:
+            raise TypeError
+
+def createLanguageString(module = None, text = None):
+    if not text:
+        text = module
+    if text:
+        return LanguageDict(text.lower(), text)
     else:
         return False
 
@@ -41,21 +104,9 @@ def getLanguage(module = None, file = None):
         else:
             filename = "%s/languages/strings.ini" % (es.getAddonPath('xa'))
     if os.path.exists(filename):
-        full = langlib.getLanguages()
-        defl = langlib.getDefaultLang()
-        lang = langlib.Strings(filename)
-        ####################################################################
-        ## ADD LANGUAGE PLACEHOLDERS FOR EASYMENUS -- BAD WAY -> OVERHEAD ##
-        ####################################################################
-        for k in lang:
-            for l in full:
-                i = full[l]["id"]
-                if not i in lang[k]:
-                    if defl in lang[k]:
-                        lang[k][i] = lang[k][defl]
-                    elif "en" in lang[k]:
-                        lang[k][i] = lang[k]["en"]
-        ####################################################################
-        return lang
+        langobj = langlib.Strings(filename)
+        for key in langobj:
+            langobj[key] = LanguageDict(key, langobj[key])
+        return langobj
     else:
         raise IOError, "Could not find %s!" % filename
