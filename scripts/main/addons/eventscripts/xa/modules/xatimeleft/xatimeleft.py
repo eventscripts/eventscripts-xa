b3fa6eb73809d150d759a3978f77f7c26fd6dfa0
freddukes <freddukes@localhost>
2009-02-04 10:34:33 +0000

    * timeleft will always return the exact time that valve sets (e.g. it's always synchronized with the server's correct timeleft) * Added function info and comments * Created a function "getTimeLeft" which can be used by other modules such as... moduleinstance.xatimeleft.getTimeLeft() to return the synchronized time * If timeleft is 0:00 it will display "This is the last round" instead of 0:00 * Fixed indentation to work with the 'default' settings (4 spaces instead of 3)

--- scripts/main/addons/eventscripts/xa/modules/xatimeleft/xatimeleft.py
+++ scripts/main/addons/eventscripts/xa/modules/xatimeleft/xatimeleft.py
@@ -7,7 +7,7 @@ from xa import xa
 #plugin information
 info = es.AddonInfo()
 info.name       = "Timeleft"
-info.version    = "1.1"
+info.version    = "1.2"
 info.author     = "Unknown"
 info.basename   = "xatimeleft"
 
@@ -17,9 +17,13 @@ info.basename   = "xatimeleft"
 # this is a global reference to your module
 xatimeleft = xa.register(info.basename)
 
-float_map_start_time = 0
+
+startTime          = 0
+playersConnected   = False
+reset              = True
 
 def load():
+    """ Initialize and register the module with XA Core """
     xatimeleft.addCommand('timeleft', timeleft_cmd, 'display_timeleft', 'UNRESTRICTED').register(('console', 'say'))
     
     """ If XA loads late, then send es_map_start event to load the start time """
@@ -28,29 +32,72 @@ def load():
 
 
 def es_map_start(event_var):
-    global float_map_start_time
+    """ Called when the map starts. Reset the start time and players connected """
+    global startTime
+    global playersConnected
+    startTime = time.time()
+    playersConnected = bool(es.getUseridList())
+    
+def round_end(event_var):
+    """
+    If the round ends with reason 16 it means the round is restarting. This resets
+    the timer, so we should make sure our timer resets too.
+    """
+    global reset
+    if event_var['reason'] == "16":
+        reset = True
 
-    float_map_start_time = time.time()
-
+def player_connect(event_var):
+    """
+    When a player activates the event player_connect for the first time, Valve
+    reset the map timer so we need to ensure that the timer is correct. We also
+    need to add 1 to the startTime as for some reason it takes a second longer
+    for the server to recognise the event.
+    """
+    global playersConnected
+    global startTime
+    if playersConnected is False:
+        startTime = (time.time() - 1)
+        playersConnected = True
+      
+def round_start(event_var):
+    """
+    When a new round starts, check to see if the previous round ended due to a
+    round restart. If so, then reset the start timer.
+    """
+    global reset
+    global startTime
+    if reset:
+        reset = False
+        startTime = time.time()
 
 def unload():
-    """ """
+    """ Unregister itself from xa core when unloaded """
     xatimeleft.unregister()
 
 
 def timeleft_cmd():
-   if not float_map_start_time:
-      return # No saved time for this map, so return
-
-   int_userid = es.getcmduserid()
-   xatimeleft.logging.log('xatimeleft request by %s (%s)' % (es.getplayersteamid(int_userid), es.getplayername(int_userid)))
-
-   float_mp_timelimit = float(es.ServerVar('mp_timelimit')) * 60
-   if float_mp_timelimit:
-      float_time_left = float_mp_timelimit - (time.time() - float_map_start_time)
-      if float_time_left < 0: float_time_left = 0
-
-      es.tell(int_userid, 'Time left in map: %s:%02d' % (int(float_time_left / 60), int(float_time_left % 60)))
-
-   else:
-      es.tell(int_userid, 'No map time limit')
+    """ 
+    Called when the 'timeleft' chat command is run. Tell the user the time
+    remaining
+    """
+    userid = es.getcmduserid()
+    xatimeleft.logging.log('xatimeleft request by %s (%s)' % (es.getplayersteamid(userid), es.getplayername(userid)))
+    timeLeft = getTimeLeft()
+    if timeLeft > -1:
+        if timeLeft:
+            es.tell(userid, 'Time left in map: %.0f:%02d' % divmod(timeLeft, 60) )
+        else:
+            es.tell(userid, 'This is the last round')
+    else:
+        es.tell(userid, 'No map time limit')
+      
+def getTimeLeft(moduleInstance = None):
+    """ A function to return the amount of time left on the server in seconds """
+    maptime = float(es.ServerVar('mp_timelimit')) * 60
+    if maptime:
+        timeLeft = maptime - (time.time() - startTime)
+        if timeLeft < 0:
+            timeLeft = 0
+        return timeLeft
+    return -1
