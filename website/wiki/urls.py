6cad5640c4e774a232a8a22ee6ad94748ec66567
ojii <ojii@localhost>
2009-12-24 18:58:43 +0100

    [XA-Website] * added (incomplete) wiki edit/create template * added bbcode documentation * added password resetting and changing templates

--- website/wiki/urls.py
+++ website/wiki/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     url(r'^(?P<name>[a-zA-Z0-9_.-]+)/?$', category, name='category'),
     url(r'^(?P<category_name>[a-zA-Z0-9_.-]+)/(?P<name>[a-zA-Z0-9_.-]+)/?$', page, name='page'),
     url(r'^(?P<category_name>[a-zA-Z0-9_.-]+)/(?P<name>[a-zA-Z0-9_.-]+)/edit/?$', edit_page, name='edit'),
+    url(r'^(?P<category_name>[a-zA-Z0-9_.-]+)/(?P<name>[a-zA-Z0-9_.-]+)/create/?$', create_page, name='create'),
 )
