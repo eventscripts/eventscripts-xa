25071fed1d04bdfbebc8cc522bba897cb3b4a9f4
ojii <ojii@localhost>
2010-01-04 09:46:47 +0900

    [xa-website] * improved German translations * Added ability to change language

--- website/home/views.py
+++ website/home/views.py
@@ -1,5 +1,8 @@
 from models import News, Release
 from xa.utils import render_to
+from django.http import HttpResponseRedirect
+from django.conf import settings
+from django.utils.translation import check_for_language
 
 @render_to
 def home(request):
@@ -16,3 +19,14 @@ def download(request, version=None):
 @render_to
 def releases(request):
     return 'home/releases.htm', {'releases': Release.objects.all()}
+    
+def set_language(request, lang):
+    next = request.META.get('HTTP_REFERER', None)
+    if not next:
+        next = '/'
+    response = HttpResponseRedirect(next)
+    if check_for_language(lang):
+        if hasattr(request, 'session'):
+            request.session['django_language'] = lang
+        response.set_cookie(settings.LANGUAGE_COOKIE_NAME,lang)
+    return response
