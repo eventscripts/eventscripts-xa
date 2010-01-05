bc6cdfd5dc314581bf2553e6109b47641dfad8dc
ojii <ojii@localhost>
2010-01-06 03:52:42 +0900

    [xa-website] * added (empty) french translation file * added some stuff in the wiki to make it downloadable (this does not work yet!)

--- website/wiki/download.py
+++ website/wiki/download.py
@@ -6,119 +6,62 @@ from settings import get
 import os
 import tarfile
 import zipfile
-import bz2
-import gzip
 try:
     from cStringIO import StringIO
 except ImportError:
     from StringIO import StringIO
-    
-    
-def myreverse(name, **kwargs):
-    """
-    My reverser
-    """
-    return reverse(name, get('WIKI_URLCON', 'xa.wiki.urls'), kwargs=kwargs)
 
 def rel_walkdir(rootdir):
     file_list = []
     for root, subfolder, files in os.walk(rootdir):
-        relroot = os.path.relpath(root, rootdir)
+        relroot = root.replace(rootdir, '')
         for file in files:
             file_list.append((os.path.join(relroot,file).lstrip('./'),
                               os.path.join(root, file)))
     return file_list
 
-def render_to_obj(*args, **kwargs):
-    """
-    Wraps render_to_string into a StringIO
-    """
-    return StringIO(render_to_string(*args, **kwargs))
-
-
-def get_gzip_obj():
-    """
-    Get a file-like object for a gzip compression
-    """
-    return gzip.GzipFile(fileobj=StringIO())
-
-def get_gzip_content(obj):
-    """
-    Get the content of a gzip file-like object
-    """
-    obj.seek(0)
-    return obj.read()
-
-def get_plain_obj():
-    """
-    Get a file-like object for a bz2 or zip compression
-    """
-    return StringIO()
-
-def get_bz2_content(obj):
-    """
-    Get the content of a bz2 file-like object
-    """
-    return bz2.compress(obj.getvalue())
-
-def get_zip_content(obj):
-    """
-    Get the content of a zip file-like object
-    """
-    return obj.getvalue()
-
-
-class Archive(object):
-    """
-    Archive baseclass
-    """
-    def __init__(self, fileobj):
-        self.fileobj = fileobj
-        
-    def get(self):
-        """
-        Get the archive (fileobj)
-        """
-        return self.fileobj
     
-    
-class TarArchive(Archive):
-    """
-    Tar Archive class
-    Compression is done in the fileobj passed.
-    """
-    def __init__(self, fileobj):
-        Archive.__init__(self, fileobj)
-        self.tar = tarfile.TarFile(fileobj=fileobj)
+class Tar(object):
+    def __init__(self, mode):
+        self.fileobj = StringIO()
+        self.tar = tarfile.open(fileobj=self.fileobj, mode='w:%s' % mode)
         
-    def add_file(self, fileobj, path):
+    def add_file(self, data, path):
         """
         Add a file to the archive
         """
         tinfo = tarfile.TarInfo(path)
-        self.tar.addfile(tinfo, fileobj)
+        tinfo.size = len(data)
+        self.tar.addfile(tinfo, StringIO(data))
         
+    def get_archive(self):
+        self.tar.close()
+        return self.fileobj.getvalue()
         
-class ZipArchive(Archive):
+
+class Zip(object):
     """
     Zip Archive class
     """
-    def __init__(self, fileobj):
-        Archive.__init__(self, fileobj)
-        self.zfile = zipfile.ZipFile(file=fileobj)
+    def __init__(self):
+        self.fileobj = StringIO()
+        self.zfile = zipfile.ZipFile(file=self.fileobj, mode='w')
         
-    def add_file(self, fileobj, path):
+    def add_file(self, data, path):
         """
         Add a file to the archive
         """
-        self.zfile.writestr(path, fileobj.read())
+        self.zfile.writestr(path, data)
+        
+    def get_archive(self):
+        self.zfile.close()
+        return self.fileobj.getvalue()
         
         
 FORMATS = {
-    'tgz': (TarArchive, get_gzip_obj, get_gzip_content, 'application/x-tar-gz'),
-    'tbz2': (TarArchive, get_plain_obj, get_bz2_content, 'application/x-bzip2'),
-    'zip': (ZipArchive, get_plain_obj, get_zip_content,
-            'application/x-zip-compressed'),
+    'tgz': (Tar, ('gz',), 'application/x-tar-gz'),
+    'tbz2': (Tar, ('bz2',), 'application/x-bzip2'),
+    'zip': (Zip, (), 'application/x-zip-compressed'),
 }
 
 
@@ -126,23 +69,23 @@ def build_download(request, lang, frmt):
     """
     Build a downloadable archive of the wiki for given language in given format.
     """
-    archive_class, opener, closer, mimetype = FORMATS[frmt]
-    archive = archive_class(opener())
+    archive_class, archive_args, mimetype = FORMATS[frmt]
+    archive = archive_class(*archive_args)
     # Get actual files
     # Add Homepage
     homepage = Page.objects.get(is_home=True)
     context = RequestContext(request, {'page': homepage,
                                        'language': lang,
                                        'is_download': True})
-    rendered = render_to_obj('wiki/page.htm', context)
-    archive.add_file(rendered, 'index.html')
+    rendered = render_to_string('wiki/page.htm', context)
+    archive.add_file(rendered, reverse('wiki:home'))
     # Add all pages
     for page in Page.objects.all():
         context = RequestContext(request, {'page': page,
                                            'language': lang,
                                            'is_download': True})
-        rendered = render_to_obj('wiki/page.htm', context)
-        path = myreverse('page', path=page.name, lang=lang)
+        rendered = render_to_string('wiki/page.htm', context)
+        path = reverse('wiki:page', kwargs={'path':page.name, 'lang':lang})
         archive.add_file(rendered, path)
     # Add category pages
     for category in Category.objects.all():
@@ -158,18 +101,20 @@ def build_download(request, lang, frmt):
                                            'pages': pages,
                                            'catobj': category,
                                            'is_download': True})
-        rendered = render_to_obj('wiki/category.htm', context)
-        path = myreverse('category', lang=lang, category=category.name)
+        rendered = render_to_string('wiki/category.htm', context)
+        path = reverse('wiki:category', kwargs={'lang': lang,
+                                                'category': category.name})
         archive.add_file(rendered, path)
     # Add media
     for media in ('js', 'ss', 'gfx'):
         rootdir = get('WIKI_%s_PATH' % media.upper())
-        prefix = get('WIKI_%S_URL_PREFIX' % media.upper())
+        prefix = get('WIKI_%s_URL_PREFIX' % media.upper())
         if not rootdir:
             continue
         for relpath, abspath in rel_walkdir(rootdir):
             path = '%s/%s' % (prefix, relpath)
             fobj = open(abspath, 'rb')
-            archive.add_file(fobj, path)
+            data = fobj.read()
             fobj.close()
-    return closer(archive), mimetype
+            archive.add_file(data, path)
+    return archive.get_archive(), mimetype
