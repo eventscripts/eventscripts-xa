d2318106c00478f94a99590c25407b1c490cbfad
errant <morton.thomas@googlemail.com>
2010-02-27 18:27:07 +0000

    Stripping out the website code and reorganising the XA code as well

--- website/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import patterns
-from views import version, gauth
-
-urlpatterns = patterns('',
-    (r'^version/$', version),
-    (r'^gauth/(?P<configid>\d+)/$', gauth),
-)
--- website/groupauth/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class EditForm(forms.Form):
-    """
-    The base groupauth edit form
-    """
-    cfgname     = forms.CharField()
-    groups      = forms.CharField()
