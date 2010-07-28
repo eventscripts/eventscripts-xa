dd1d8e3324ff535bcb84759f818fd575b7f1a308
mback2k <mback2k@localhost>
2010-07-28 15:54:23 +0200

    XA High Ping Kick: Fixed repeat handling to avoid exceptions after map change

--- eventscripts/main/addons/eventscripts/xa/modules/xahighpingkick/xahighpingkick.py
+++ eventscripts/main/addons/eventscripts/xa/modules/xahighpingkick/xahighpingkick.py
@@ -6,11 +6,11 @@ from xa import xa
 #plugin information
 info = es.AddonInfo() 
 info.name     = 'High Ping Kicker' 
-info.version  = '1.2'
+info.version  = '1.2a'
 info.author   = 'SumGuy14'
 info.basename = 'xahighpingkick' 
 
-gInfo = {}
+gUserid = {}
 
 # register module with XA
 xahighpingkick = xa.register(info.basename)
@@ -26,33 +26,34 @@ exceedlimit   = xahighpingkick.setting.createVariable('ping_exceedlimit', 3, 'If
 
 def load():
     """ Make sure all people on the server when this is loaded are not forgotten """
-    for userid in map(str, es.getUseridList()):
-        player_activate({'userid':userid})
+    for userid in playerlib.getUseridList('#human'):
+        player_activate({'userid': userid})
 
 def unload(): 
-    for userid in map(str, es.getUseridList()):
-        if repeat.status('hpk_track_' + str(userid)) != 0:
-            repeat.find('hpk_track_' + str(userid)).delete()
-    xahighpingkick.unregister() 
+    for userid in gUserid:
+        player_disconnect({'userid': userid})
+    xahighpingkick.unregister()
 
 def player_activate(event_var):
-    userid = event_var['userid']
-    gInfo[userid] = 0
-    repeat.create('hpk_track_' + userid,tracker,userid).start(float(interval),float(check))
+    userid = int(event_var['userid'])
+    if not userid in gUserid:
+        player = playerlib.getPlayer(userid)
+        if not player.isbot:
+            player._hpk_violations = 0
+            gUserid[userid] = repeat.create('hpk_track_%s' % userid, _tracker, player)
+            gUserid[userid].start(float(interval), float(check))
 
 def player_disconnect(event_var):
-    userid = event_var['userid']
-    if repeat.status('hpk_track_' + userid) != 0:
-        repeat.find('hpk_track_' + userid).delete()
+    userid = int(event_var['userid'])
+    if userid in gUserid:
+        gUserid[userid].delete()
+        del gUserid[userid]
 
-def tracker(userid, info = None):
-    if es.exists('userid',userid):
-        ping = es.createplayerlist(userid)[int(userid)]['ping']
-        if maxping and ping >= int(maxping):
-            gInfo[userid]+=1
-        if exceedlimit and gInfo[userid] >= int(exceedlimit):
-            slowguy = playerlib.getPlayer(userid)
-            slowguy.kick(reason=text('kick', {}, slowguy.get('lang')))
-            xahighpingkick.logging.log("kicked for breaking the ping limit", userid)
-    elif info is not None:
-        info[0].delete()
+def _tracker(player):
+    if player.isOnline():
+        player.refreshAttributes()
+        if int(maxping) and player.ping >= int(maxping):
+            player._hpk_violations += 1
+        if int(exceedlimit) and player._hpk_violations >= int(exceedlimit):
+            player.kick(reason=text('kick', {}, player.lang))
+            xahighpingkick.logging.log('Kicked for breaking the high ping limit', userid)
